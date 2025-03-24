af7d7deb1a6c2c663ce8990b5db4cd57c5743844
Cameron Davis <149528769+camdav050696@users.noreply.github.com>
2025-03-24 15:22:59 -0400

    Add files via upload

---
+++ sorting hat practice.py
@@ -0,0 +1,11 @@
+### create a sorting hat quiz from memory of summer studies
+
+# display welcome msg from Black Hogwarts
+
+print("Welcome to Black Hogwarts (Copyright)")
+
+
+# define variable for 'age'
+age = input("First, enter your age: ")
+print("First, enter your age: ")
+print(age)
